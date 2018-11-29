40acc1e8879794313467d640b7c69a292dc9f851
Michael Cao <mike.x.cao@gmail.com>
2018-11-28 21:52:43 -0500

    introductions

--- commands/informational/intro.js
+++ commands/informational/intro.js
@@ -12,7 +12,60 @@ class intro extends Command {
   }
 
   async run(bot, msg, args, level) {
-    // eslint-disable-line no-unused-vars
+    let { MessageEmbed } = require("discord.js");
+    if (!msg.mentions.users.first()) {
+      let user = await bot.database.users.get(msg.author.id);
+      let discorduser = msg.author;
+      let discordmember = msg.member;
+      let intro = user.intro || null;
+      if (intro) {
+        let embed = new MessageEmbed()
+          .setAuthor(
+            "Introduction: " + discorduser.tag,
+            discorduser.avatarURL()
+          )
+          .setColor(
+            discordmember.displayHexColor == "#000000"
+              ? null
+              : discordmember.displayHexColor
+          )
+          .setDescription(intro)
+          .setThumbnail(discorduser.avatarURL())
+          .setTimestamp()
+          .setFooter(msg.guild.name, msg.guild.iconURL());
+
+        msg.channel.send("<@" + discorduser + ">'s Intro:", embed);
+      } else {
+        msg.reply(
+          "you do not have an intro yet! Please add one using the `addintro` comnmand."
+        );
+      }
+    } else {
+      let user = await bot.database.users.get(msg.mentions.users.first().id);
+      let intro = user.intro || null;
+      if (intro) {
+        let discorduser = msg.mentions.users.first();
+        let discordmember = msg.mentions.members.first();
+        let embed = new MessageEmbed()
+          .setAuthor(
+            "Introduction: " + discorduser.tag,
+            discorduser.avatarURL()
+          )
+          .setColor(
+            discordmember.displayHexColor == "#000000"
+              ? null
+              : discordmember.displayHexColor
+          )
+          .setDescription(intro)
+          .setThumbnail(discorduser.avatarURL())
+          .setTimestamp()
+          .setFooter(msg.guild.name, msg.guild.iconURL());
+
+        msg.channel.send("<@" + discorduser + ">'s Intro:", embed);
+      } else {
+        msg.reply("they do not have an intro yet!");
+      }
+    }
   }
 }
 
