2681064f1dd8c85e99e8d10530f5fa9b3a48fc68
Michael Cao <mike.x.cao@gmail.com>
2018-12-28 16:40:14 -0500

    add ready event

---
+++ events/ready.js
@@ -0,0 +1,53 @@
+module.exports = class {
+  constructor(bot) {
+    this.bot = bot;
+  }
+
+  async run(bot) {
+    let schedule = require("node-schedule");
+
+    setTimeout(function() {
+      bot.logger.ready(
+        bot.user.username +
+          " is ready! Serving " +
+          bot.guilds.size +
+          " servers."
+      );
+      bot.user.setActivity("with subtle asian traits", {
+        type: "PLAYING"
+      });
+
+      function birthdays() {
+        bot.users.get(bot.config.Discord.ownerID).send("Checking Birthdays");
+        let d = new Date();
+        bot.guilds
+          .get("516463112980004875")
+          .roles.get("517244087817076738")
+          .members.forEach(member => {
+            member.roles.remove("517244087817076738");
+          });
+        bot.database.users
+          .filter({
+            birthday: { day: d.getDate(), month: d.getMonth() + 1 }
+          })
+          .then(bday => {
+            if (bday.length > 0) {
+              for (var i = 0; i < bday.length; i++) {
+                let user = bot.guilds
+                  .get("516463112980004875")
+                  .members.get(bday[i].id);
+                if (user) {
+                  user.roles.add("517244087817076738");
+                  user.user.send(
+                    "Since today is your birthday, you have been given a special role in Subtle Asian Discord! :tada:"
+                  );
+                }
+              }
+            }
+          });
+      }
+
+      bot.birthdays = schedule.scheduleJob("* * 1 * *", birthdays());
+    }, 1000);
+  }
+};
