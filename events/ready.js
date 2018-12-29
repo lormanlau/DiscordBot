8548fb292dcb5a8f9de3ea603a73e91e6c67874a
Michael Cao <mike.x.cao@gmail.com>
2018-12-29 11:17:21 -0500

    ready updates

--- events/ready.js
+++ events/ready.js
@@ -17,8 +17,32 @@ module.exports = class {
         type: "PLAYING"
       });
 
+      let cachedMessages = [];
+      bot.reactionMessages = {};
+      bot.database.reactions.then(async reactions => {
+        for (var i = 0; i < reactions.length; i++) {
+          for (let channel in reactions[i]) {
+            if (channel != "id") {
+              for (let message in reactions[i][channel]) {
+                await bot.channels.get(channel).messages.fetch(message);
+                cachedMessages.push(message);
+                bot.reactionMessages[message] = reactions[i][channel][message];
+              }
+            }
+          }
+        }
+
+        bot.users
+          .get(bot.config.Discord.ownerID)
+          .send(
+            "Fetched the following messages: ```" +
+              JSON.stringify(cachedMessages, null, "\t") +
+              "```"
+          );
+      });
+
       function birthdays() {
-        bot.users.get(bot.config.Discord.ownerID).send("Checking Birthdays");
+        // bot.users.get(bot.config.Discord.ownerID).send("Checking Birthdays");
         let d = new Date();
         bot.guilds
           .get("516463112980004875")
@@ -32,17 +56,26 @@ module.exports = class {
           })
           .then(bday => {
             if (bday.length > 0) {
+              let bdays = [];
               for (var i = 0; i < bday.length; i++) {
                 let user = bot.guilds
                   .get("516463112980004875")
                   .members.get(bday[i].id);
                 if (user) {
+                  bdays.push(user.user.tag);
                   user.roles.add("517244087817076738");
                   user.user.send(
                     "Since today is your birthday, you have been given a special role in Subtle Asian Discord! :tada:"
                   );
                 }
               }
+              bot.users
+                .get(bot.config.Discord.ownerID)
+                .send(
+                  "The following users have been given the birthday role: ```" +
+                    bdays.join(", ") +
+                    "```"
+                );
             }
           });
       }
