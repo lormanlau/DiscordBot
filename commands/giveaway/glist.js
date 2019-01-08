7e19603277fec261ecc32ee8417835e8f69a4b55
Lorman Lau <lormanlau@gmail.com>
2019-01-08 15:49:06 -0800

    Edited glist

--- commands/giveaway/glist.js
+++ commands/giveaway/glist.js
@@ -11,12 +11,33 @@ class glist extends Command {
     });
   }
 
+  timeConverter(time){
+    var dayInMilliseconds = 24 * 60 * 60 * 1000;
+    var hourInMilliseconds = 60 * 60 * 1000;
+    var minuteInMilliseconds = 60 * 1000;
+    var days = Math.floor(time/dayInMilliseconds);
+    var hours = Math.floor((time - (days * dayInMilliseconds))/hourInMilliseconds);
+    var minutes = Math.floor((time - days * dayInMilliseconds - hours * hourInMilliseconds) / minuteInMilliseconds);
+    var seconds = Math.round((time - days * dayInMilliseconds - hours * hourInMilliseconds - minutes * minuteInMilliseconds)/ 1000);
+    var output = "";
+    if (days != 0)
+      output += `**${days}** day(s), `;
+    if (hours != 0)
+      output += `**${hours}** hour(s), `;
+    if (minutes != 0)
+      output += `**${minutes}** minute(s), `;
+    output += `**${seconds}** second(s)`;
+    return output;
+  }
+
   async run(bot, msg, args, level) {
-    let dbGList = await bot.database.giveaways.filter({winner_object: [], guildID: msg.guild.id});
-    if (dbGList.length > 0) {
-      msg.reply(`There are current ${dbGList.length} giveaways active.`);
-    } else {
-      msg.reply("Currently no Giveaways active.");
+    var activeGiveaways = msg.guild.giveaways;
+    if (!activeGiveaways)
+      return msg.channel.send("Currently No active Giveaways. :(")
+    for (var i = 0; i < activeGiveaways.length; i++) {
+      var channel = msg.guild.channels.filter(channel => channel.id == activeGiveaways[i].giveaway.channelID).first()
+      var diff = activeGiveaways[i].giveaway.endTime - new Date();
+      msg.channel.send(`${activeGiveaways[i].giveaway.id} | ${channel} | **${activeGiveaways[i].giveaway.winners}** winner(s) | Prize: **${activeGiveaways[i].giveaway.prize}** | Ends in ${this.timeConverter(diff)}`)
     }
   }
 }
